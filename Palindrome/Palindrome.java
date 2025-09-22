c8c54a0effedc6a7b64b76b09b16f66c1381011b
lfarssi <medlfarssi10@gmail.com>
2025-09-22 13:36:20 +0100

    Palindrome.java

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,6 +1,8 @@
 public class Palindrome {
     public static Boolean isPalindrome(String str) {
-
+        if (str == null) {
+            return false;
+        }
         StringBuilder reversed = new StringBuilder(str.toLowerCase()).reverse();
         return  (str==null) ?false: str.toLowerCase().equals(reversed.toString());
     }
