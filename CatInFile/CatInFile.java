0dc00a1bf53aeab0113956641881c7026e154e4e
lfarssi <medlfarssi10@gmail.com>
2025-09-23 11:48:59 +0100

    cat

---
+++ CatInFile/CatInFile.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CatInFile {
+    public static void cat(String[] args) throws IOException {
+ if (args.length !=1){
+            return ;
+        }
+        String filename= args[0];
+        Path path= Path.of(filename);
+        try {
+            byte[] content = System.in.readAllBytes();
+            Files.write(path,content);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } 
+    }
+}   
