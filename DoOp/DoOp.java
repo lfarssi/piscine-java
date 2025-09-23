1c7c7d5d35c975e0a4177c983ed5e728458316e0
lfarssi <medlfarssi10@gmail.com>
2025-09-23 10:52:25 +0100

    DoOp

---
+++ DoOp/DoOp.java
@@ -0,0 +1,14 @@
+public class DoOp {
+    public static String operate(String[] args) {
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[2]);
+        String op = args[1];
+        return switch(op){
+            case "+" -> String.valueOf(n1 + n2);
+            case "-" -> String.valueOf(n1 - n2);
+            case "*" -> String.valueOf(n1 * n2);
+            case "/" -> String.valueOf(n1 / n2);
+            default ->"Error";
+        };
+    }
+}
