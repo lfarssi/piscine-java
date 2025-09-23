d0eb12e30674f5a271c8851c0588a2eea8ec40ee
lfarssi <medlfarssi10@gmail.com>
2025-09-23 13:47:51 +0100

    capitalize

---
+++ FileManager/FileManager.java
@@ -0,0 +1,25 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.*;
+
+public class FileManager {
+    public static void createFile(String fileName, String content) throws IOException {
+        Path path= Paths.get(fileName);
+        Files.createFile(path);
+        Files.write(path,content.getBytes());
+    }
+    public static String getContentFile(String fileName) throws IOException {
+        Path path = Path.of(fileName);
+        String content = Files.readString(path);
+        return content;
+    }
+    public static void deleteFile(String fileName) {
+        Path path = Path.of(fileName);
+        try{
+            Files.delete(path);
+        } catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
