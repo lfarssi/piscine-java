5e1a9bcc6742676ba62d5bce812d07af1a46d210
lfarssi <medlfarssi10@gmail.com>
2025-09-25 12:38:32 +0100

    equals

--- StarUtils/CelestialObject.java
+++ StarUtils/CelestialObject.java
@@ -73,7 +73,7 @@ public class CelestialObject {
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
-      return false;
+      return true;
     }
     if (obj == null || getClass() != obj.getClass()) {
             return false;
