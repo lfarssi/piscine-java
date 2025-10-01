78e8a43f2c625fdbc870d45ea0faca0d88677b0e
lfarssi <medlfarssi10@gmail.com>
2025-10-01 11:11:41 +0100

    index

---
+++ ListSearchIndex/ListSearchIndex.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListSearchIndex {
+
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        return list.lastIndexOf(value);
+    }
+
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        return list.indexOf(value);
+    }
+
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        List<Integer> indexes = new ArrayList<>();
+        
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(value)) {
+                indexes.add(i);  
+            }
+        }
+        
+        return indexes;
+    }
+}
