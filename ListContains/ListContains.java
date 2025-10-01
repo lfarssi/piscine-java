1f8753cfe2f01a391ff046aea55584b86ed6b275
lfarssi <medlfarssi10@gmail.com>
2025-10-01 11:01:15 +0100

    list contains

---
+++ ListContains/ListContains.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class ListContains {
+    public static boolean containsValue(List<Integer> list, Integer value) {
+        // your code here
+        return list.contains(value);
+    }
+}
